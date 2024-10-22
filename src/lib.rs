b1bf345987fb7765de7e10c462b268ea68eda7f3
RGGH <pisscompany@gmail.com>
2024-10-22 08:19:09 +0100

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,49 @@ pub fn calculate_sdlt(property_value: f64) -> f64 {
         portion1 + portion2 + portion3
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sdlt_below_250k() {
+        let property_value = 200000.0;
+        let expected = 0.0;
+        assert_eq!(calculate_sdlt(property_value), expected);
+    }
+
+    #[test]
+    fn test_sdlt_at_250k() {
+        let property_value = 250000.0;
+        let expected = 0.0;
+        assert_eq!(calculate_sdlt(property_value), expected);
+    }
+
+    #[test]
+    fn test_sdlt_between_250k_and_925k() {
+        let property_value = 500000.0;
+        let expected = (500000.0 - 250000.0) * 0.05;
+        assert_eq!(calculate_sdlt(property_value), expected);
+    }
+
+    #[test]
+    fn test_sdlt_between_925k_and_1_5m() {
+        let property_value = 1000000.0;
+        let portion1 = (925000.0 - 250000.0) * 0.05;
+        let portion2 = (1000000.0 - 925000.0) * 0.10;
+        let expected = portion1 + portion2;
+        assert_eq!(calculate_sdlt(property_value), expected);
+    }
+
+    #[test]
+    fn test_sdlt_above_1_5m() {
+        let property_value = 2000000.0;
+        let portion1 = (925000.0 - 250000.0) * 0.05;
+        let portion2 = (1500000.0 - 925000.0) * 0.10;
+        let portion3 = (2000000.0 - 1500000.0) * 0.12;
+        let expected = portion1 + portion2 + portion3;
+        assert_eq!(calculate_sdlt(property_value), expected);
+    }
+}
+
